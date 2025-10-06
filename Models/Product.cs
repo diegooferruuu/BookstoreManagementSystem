a0ad872c4fc850144dbe2d4ef0e4bd31486749e6
Mcqueen <camilo.padilla@ucb.edu.bo>
2025-10-05 20:24:40 -0400

    Model and IDataBase Implementations

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace BookstoreManagementSystem.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Categoria { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+        public DateTime CreadoEn { get; set; }
+
+    }
+}

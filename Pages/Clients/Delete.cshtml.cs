dabc63cc9631d91d673d9046d790784c3a3f4d6a
Diego Ferrufino <diego.ferrufino@ucb.edu.bo>
2025-10-06 22:52:26 -0400

    Clients graphic implementation

--- Pages/Clients/Delete.cshtml.cs
+++ Pages/Clients/Delete.cshtml.cs
@@ -1,27 +1,39 @@
 using BookstoreManagementSystem.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BookstoreManagementSystem.Models;
+using BookstoreManagementSystem.Services;
+
 
 namespace BookstoreManagementSystem.Pages.Clients
 {
     public class DeleteModel : PageModel
     {
-        private readonly ClientRepository _repo = new ClientRepository();
+        private readonly IDataBase<Client> _repository;
 
         [BindProperty]
-        public int Id { get; set; }
+        public Client Client { get; set; } = new();
+
+        public DeleteModel()
+        {
+            var creator = new ClientCreator();
+            _repository = creator.FactoryMethod();
+        }
 
         public IActionResult OnGet(int id)
         {
-            Id = id;
+            Client = _repository.Read(id);
+
+            if (Client == null)
+                return RedirectToPage("Index");
+
             return Page();
         }
 
         public IActionResult OnPost()
         {
-            _repo.Delete(Id);
+            _repository.Delete(Client.Id);
             return RedirectToPage("Index");
         }
-
     }
 }
--- Pages/Clients/Index.cshtml.cs
+++ Pages/Clients/Index.cshtml.cs
@@ -2,25 +2,26 @@ using BookstoreManagementSystem.Models;
 using BookstoreManagementSystem.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BookstoreManagementSystem.Services;
+
 
 namespace BookstoreManagementSystem.Pages.Clients
 {
     public class IndexModel : PageModel
     {
-        public List<Client> Clientes { get; private set; }
+        private readonly IDataBase<Client> _repository;
+
+        public List<Client> Clients { get; set; } = new();
+
+        public IndexModel()
+        {
+            var creator = new ClientCreator();
+            _repository = creator.FactoryMethod();
+        }
 
         public void OnGet()
         {
-            var repo = new ClientRepository();
-            Clientes = new List<Client>();
-
-            for (int i = 1; i <= 10; i++) // Simulación: leer los primeros 10 clientes
-            {
-                var cliente = repo.Read(i);
-                if (cliente != null)
-                    Clientes.Add(cliente);
-            }
-
+            Clients = _repository.GetAll();
         }
     }
 }
